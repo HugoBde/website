df3618f183e5e4dec3990456c1644087d14581fd
Hugo Bouderlique <bouderliqueh@gmail.com>
2024-05-05 00:06:42 +1000

    refactor article resumption/building + add blog source watcher to build / update / remove as changes are made

--- cmd/backend.go
+++ cmd/backend.go
@@ -4,10 +4,7 @@ import (
 	"log"
 	"net/http"
 	"os"
-	"path"
-	"slices"
 	"strconv"
-	"strings"
 
 	"hugobde.dev/internal/article"
 	"hugobde.dev/internal/templates"
@@ -16,15 +13,9 @@ import (
 	"github.com/joho/godotenv"
 )
 
-// static file server
-
-// blog articles list page
-
-// blog page builder
-
-var BLOG_ARTICLES []*article.BlogArticle = make([]*article.BlogArticle, 0)
-
 func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
 	err := godotenv.Load()
 	if err != nil {
 		log.Println("Failed to load .env file, hope you have environments variables already set :)")
@@ -46,16 +37,17 @@ func main() {
 
 	}
 
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	builder, err := article.NewArticleBuilderWatcher("./website/blog_source", "./website/blog")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	go builder.Run()
 
 	http.HandleFunc("/", homePage)
-	http.HandleFunc("/blog", blogPage)
-	http.HandleFunc("/blog/{index}", articlePage)
+	http.HandleFunc("/blog", blogPageHandler(builder))
+	http.HandleFunc("/blog/{index}", articlePageHandler(builder))
 	http.Handle("/static/", http.StripPrefix("/static", http.FileServer(http.Dir("./website/static"))))
-
-	// go articleBuilder("./blog/source", "./blog")
-
-	articlesResolve("./website/blog_source", "./website/blog")
 
 	log.Println("Listening on :" + port)
 	log.Fatal(http.ListenAndServeTLS(":"+port, serverCert, serverKey, nil))
@@ -65,99 +57,27 @@ func homePage(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, "./website/index.html")
 }
 
-func blogPage(w http.ResponseWriter, r *http.Request) {
-	templates.BlogTemplate(BLOG_ARTICLES).Render(r.Context(), w)
+func blogPageHandler(b *article.ArticleBuilderWatcher) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		templates.BlogTemplate(b.Articles).Render(r.Context(), w)
+	}
 }
 
-func articlePage(w http.ResponseWriter, r *http.Request) {
-	articleIndex, err := strconv.Atoi(r.PathValue("index"))
-	if err != nil {
-		w.WriteHeader(http.StatusNotFound)
-	}
-	templates.ArticlePage(BLOG_ARTICLES[articleIndex]).Render(r.Context(), w)
-}
-
-func articlesResolve(sourceDir string, articleDir string) {
-	// Get entries in blog source dir
-	sourceDirEntries, err := os.ReadDir(sourceDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Get entries in compiled articles dir
-	articleDirEntries, err := os.ReadDir(articleDir)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Resolve each blog source with its compiled article
-	for _, sourceDirEntry := range sourceDirEntries {
-		// Get article name, and ignore entries without .md extension
-		articleName, ok := strings.CutSuffix(sourceDirEntry.Name(), ".md")
-		if !ok {
-			continue
-		}
-
-		// Look up matching compiled article
-		articleDirEntry, ok := findArticleFile(articleName, articleDirEntries)
-		if !ok {
-			log.Println("Building article", articleName)
-			article, err := article.BuildArticleFromSourceFile(articleName, path.Join(sourceDir, sourceDirEntry.Name()))
-			if err != nil {
-				log.Fatal(err)
-			}
-			os.WriteFile(path.Join(articleDir, articleName+".html"), article.HTML, 0644)
-			BLOG_ARTICLES = append(BLOG_ARTICLES, article)
-			continue
-		}
-
-		// Get both files info
-		articleFileInfo, err := articleDirEntry.Info()
-		sourceFileInfo, err := sourceDirEntry.Info()
+func articlePageHandler(b *article.ArticleBuilderWatcher) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		articleIndex, err := strconv.Atoi(r.PathValue("index"))
 		if err != nil {
-			log.Fatal(err)
+			w.WriteHeader(http.StatusNotFound)
 		}
 
-		// If the compiled info is older than the source, rebuild
-		if sourceFileInfo.ModTime().Sub(articleFileInfo.ModTime()) > 0 {
-			log.Println("Updating article", articleName)
-			article, err := article.BuildArticleFromSourceFile(articleName, path.Join(sourceDir, sourceDirEntry.Name()))
-			if err != nil {
-				log.Fatal(err)
-			}
-			os.WriteFile(path.Join(articleDir, articleName+".html"), article.HTML, 0644)
-			BLOG_ARTICLES = append(BLOG_ARTICLES, article)
-			continue
+		if articleIndex >= len(b.Articles) {
+			w.WriteHeader(http.StatusNotFound)
+			w.Write([]byte("how did you get here buddy?"))
+			return
 		}
 
-		log.Println("Loading article", articleName)
-		article, err := article.ReadArticleFromFile(articleName, path.Join(articleDir, articleFileInfo.Name()), sourceFileInfo.ModTime())
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		BLOG_ARTICLES = append(BLOG_ARTICLES, article)
+		templates.ArticlePage(b.Articles[articleIndex]).Render(r.Context(), w)
 	}
-
-	// Remove articles for which the source is missing
-	for _, articleDirEntry := range articleDirEntries {
-		articleName, ok := strings.CutSuffix(articleDirEntry.Name(), ".html")
-		if !ok {
-			continue
-		}
-
-		_, ok = findArticleFile(articleName, sourceDirEntries)
-		if ok {
-			continue
-		}
-
-		os.Remove(path.Join(articleDir, articleDirEntry.Name()))
-	}
-
-	// Sort articles in reverse chronological order
-	slices.SortFunc[[]*article.BlogArticle](BLOG_ARTICLES, func(a *article.BlogArticle, b *article.BlogArticle) int {
-		return int(b.LastModifiedTime.Sub(a.LastModifiedTime))
-	})
 }
 
 func articleBuilder(sourceDir string, articleDir string) {
@@ -170,14 +90,4 @@ func articleBuilder(sourceDir string, articleDir string) {
 	if err != nil {
 		log.Fatal(err)
 	}
-}
-
-func findArticleFile(articleName string, entries []os.DirEntry) (os.DirEntry, bool) {
-	for _, entry := range entries {
-		if strings.HasPrefix(entry.Name(), articleName) {
-			return entry, true
-		}
-	}
-
-	return nil, false
 }
--- internal/article/article.go
+++ internal/article/article.go
@@ -1,10 +1,15 @@
 package article
 
 import (
+	"fmt"
+	"log"
 	"os"
+	"path"
+	"slices"
 	"strings"
 	"time"
 
+	"github.com/fsnotify/fsnotify"
 	"github.com/gomarkdown/markdown"
 )
 
@@ -14,8 +19,186 @@ type BlogArticle struct {
 	Title            string
 }
 
-func BuildArticleFromSourceFile(title string, sourceFile string) (*BlogArticle, error) {
-	source, err := os.ReadFile(sourceFile)
+type ArticleBuilderWatcher struct {
+	Articles  []*BlogArticle
+	watcher   *fsnotify.Watcher
+	sourceDir string
+	outputDir string
+}
+
+func NewArticleBuilderWatcher(sourceDir string, outputDir string) (*ArticleBuilderWatcher, error) {
+	watcher, err := fsnotify.NewWatcher()
+	if err != nil {
+		return nil, err
+	}
+
+	err = watcher.Add(sourceDir)
+	if err != nil {
+		return nil, err
+	}
+
+	return &ArticleBuilderWatcher{
+		Articles:  make([]*BlogArticle, 0),
+		watcher:   watcher,
+		sourceDir: sourceDir,
+		outputDir: outputDir,
+	}, nil
+}
+
+func (b *ArticleBuilderWatcher) Run() {
+	b.resume()
+	b.watch()
+}
+
+func (b *ArticleBuilderWatcher) resume() {
+
+	// Get entries in blog source dir
+	sourceDirEntries, err := os.ReadDir(b.sourceDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Get entries in compiled articles dir
+	compiledDirEntries, err := os.ReadDir(b.outputDir)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Resolve each blog source with its compiled article
+	for _, sourceDirEntry := range sourceDirEntries {
+		if !strings.HasSuffix(sourceDirEntry.Name(), ".md") {
+			continue
+		}
+
+		// Get article name, and ignore entries without .md extension
+		title := strings.TrimSuffix(sourceDirEntry.Name(), ".md")
+
+		// Find a matching compiled article
+		articleDirEntry := findDirEntry(title, compiledDirEntries)
+
+		// If no match found, build the article
+		if articleDirEntry == nil {
+			article, err := b.buildArticle(sourceDirEntry.Name())
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+
+			os.WriteFile(path.Join(b.outputDir, title+".html"), article.HTML, 0644)
+			b.Articles = append(b.Articles, article)
+			continue
+		}
+
+		// If we find a match, get both files info to compare last modified time
+		articleFileInfo, err := articleDirEntry.Info()
+		sourceFileInfo, err := sourceDirEntry.Info()
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		// If the compiled file info is older than the source, rebuild
+		if sourceFileInfo.ModTime().Sub(articleFileInfo.ModTime()) > 0 {
+			article, err := b.buildArticle(sourceDirEntry.Name())
+			if err != nil {
+				log.Println(err)
+				continue
+			}
+			os.WriteFile(path.Join(b.outputDir, title+".html"), article.HTML, 0644)
+			b.Articles = append(b.Articles, article)
+			continue
+		}
+
+		article, err := b.loadArticle(articleFileInfo.Name(), sourceFileInfo.ModTime())
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		b.Articles = append(b.Articles, article)
+		continue
+	}
+
+	// Remove articles for which the source is missing
+	for _, compiledDirEntry := range compiledDirEntries {
+		title, ok := strings.CutSuffix(compiledDirEntry.Name(), ".html")
+		if !ok {
+			continue
+		}
+
+		sourceDirEntry := findDirEntry(title, sourceDirEntries)
+		if sourceDirEntry != nil {
+			continue
+		}
+
+		os.Remove(path.Join(b.outputDir, compiledDirEntry.Name()))
+	}
+
+	// Sort articles in reverse chronological order
+	slices.SortFunc[[]*BlogArticle](b.Articles, func(a *BlogArticle, b *BlogArticle) int {
+		return int(b.LastModifiedTime.Sub(a.LastModifiedTime))
+	})
+
+	log.Printf("Resumption complete: %d article(s)", len(b.Articles))
+}
+
+func (b *ArticleBuilderWatcher) watch() {
+	for {
+		event := <-b.watcher.Events
+
+		// ignore changes to non .md files
+		if !strings.HasSuffix(event.Name, ".md") {
+			continue
+		}
+
+		switch event.Op {
+		case fsnotify.Write:
+			article, err := b.buildArticle(path.Base(event.Name))
+			if err != nil {
+				log.Println(err)
+			}
+			if oldArticle := b.findArticle(article.Title); oldArticle != nil {
+				b.updateArticle(article)
+			} else {
+				b.Articles = append(b.Articles, article)
+			}
+
+		case fsnotify.Remove:
+			b.removeArticle(path.Base(event.Name))
+
+		}
+
+		slices.SortFunc[[]*BlogArticle](b.Articles, func(a *BlogArticle, b *BlogArticle) int {
+			return int(b.LastModifiedTime.Sub(a.LastModifiedTime))
+		})
+	}
+}
+
+// Load an already compiled file
+func (b *ArticleBuilderWatcher) loadArticle(compiledFile string, lastModifiedTime time.Time) (*BlogArticle, error) {
+	title := strings.ReplaceAll(strings.TrimSuffix(compiledFile, ".html"), "_", " ")
+
+	log.Printf("Loading article \"%s\"", title)
+
+	html, err := os.ReadFile(path.Join(b.outputDir, compiledFile))
+	if err != nil {
+		return nil, err
+	}
+
+	article := BlogArticle{
+		Title:            strings.ReplaceAll(title, "_", " "),
+		HTML:             html,
+		LastModifiedTime: lastModifiedTime,
+	}
+
+	return &article, nil
+}
+
+// Build an article from a markdown source file
+func (b *ArticleBuilderWatcher) buildArticle(sourceFile string) (*BlogArticle, error) {
+	title := strings.ReplaceAll(strings.TrimSuffix(sourceFile, ".md"), "_", " ")
+
+	log.Printf("Building article \"%s\"", title)
+
+	source, err := os.ReadFile(path.Join(b.sourceDir, sourceFile))
 	if err != nil {
 		return nil, err
 	}
@@ -29,17 +212,46 @@ func BuildArticleFromSourceFile(title string, sourceFile string) (*BlogArticle, 
 	return &article, nil
 }
 
-func ReadArticleFromFile(title string, filename string, lastModifiedTime time.Time) (*BlogArticle, error) {
-	html, err := os.ReadFile(filename)
-	if err != nil {
-		return nil, err
+// Look for article with the same title in the list of existing articles and replace it with the new one
+func (b *ArticleBuilderWatcher) updateArticle(newArticle *BlogArticle) bool {
+	for i, a := range b.Articles {
+		if a.Title == newArticle.Title {
+			b.Articles[i] = newArticle
+			return true
+		}
 	}
 
-	article := BlogArticle{
-		Title:            strings.ReplaceAll(title, "_", " "),
-		HTML:             html,
-		LastModifiedTime: lastModifiedTime,
-	}
+	return false
+}
 
-	return &article, nil
+func (b *ArticleBuilderWatcher) removeArticle(sourceFile string) error {
+	title := strings.ReplaceAll(strings.TrimSuffix(sourceFile, ".md"), "_", " ")
+
+	log.Printf("Removing article \"%s\"", title)
+
+	for i, a := range b.Articles {
+		if a.Title == title {
+			b.Articles = append(b.Articles[:i], b.Articles[i+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("Tried to delete article %s which is not in the list ???", title)
+}
+
+func (b *ArticleBuilderWatcher) findArticle(title string) *BlogArticle {
+	for _, a := range b.Articles {
+		if a.Title == title {
+			return a
+		}
+	}
+	return nil
+}
+
+func findDirEntry(title string, dirEntries []os.DirEntry) os.DirEntry {
+	for _, dirEntry := range dirEntries {
+		if strings.HasPrefix(dirEntry.Name(), title) {
+			return dirEntry
+		}
+	}
+	return nil
 }
